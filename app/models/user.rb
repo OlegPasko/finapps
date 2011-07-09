277dd5e3b66d00669ad2e08a151486b7a54bad33
OlegPasko <olegpasko@gmail.com>
2011-07-10 00:57:33 +0300

    present validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
-  validates_presence_of :password, :on => :create
+  validates_presence_of :name, :email, :password, :on => :create
   validates_uniqueness_of :name, :email
 end
