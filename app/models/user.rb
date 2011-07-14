0dd5fecbdd3b315a1c5e24511d693356643cf005
OlegPasko <olegpasko@gmail.com>
2011-07-15 01:47:19 +0300

    email regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates_presence_of :name, :email, :password, :on => :create
   validates_uniqueness_of :name, :email
+  validates :email, :format => { :with => email_regex }
 end
